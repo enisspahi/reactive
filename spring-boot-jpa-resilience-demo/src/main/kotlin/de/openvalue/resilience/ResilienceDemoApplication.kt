c3478dfcecf83c6766637eb780d45b1713792551
enisspahi <enis.spahi@openvalue.de>
2023-03-29 10:22:15 +0200

    at-last-once-delivery guarantee with stream processing

--- spring-boot-jpa-resilience-demo/src/main/kotlin/de/openvalue/resilience/ResilienceDemoApplication.kt
+++ spring-boot-jpa-resilience-demo/src/main/kotlin/de/openvalue/resilience/ResilienceDemoApplication.kt
@@ -2,12 +2,14 @@ package de.openvalue.resilience
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 import org.springframework.scheduling.annotation.EnableAsync
 
 @SpringBootApplication
 @EnableAsync
-class ResilienceDemoDemoApplication
+@EnableJpaAuditing
+class ResilienceDemoApplication
 
 fun main(args: Array<String>) {
-	runApplication<ResilienceDemoDemoApplication>(*args)
+	runApplication<ResilienceDemoApplication>(*args)
 }
--- spring-boot-jpa-resilience-demo/src/main/kotlin/de/openvalue/resilience/application/OrderService.kt
+++ spring-boot-jpa-resilience-demo/src/main/kotlin/de/openvalue/resilience/application/OrderService.kt
@@ -2,19 +2,15 @@ package de.openvalue.resilience.application
 
 import de.openvalue.resilience.adapter.repository.OrderRepository
 import de.openvalue.resilience.domain.OrderEntity
-import de.openvalue.resilience.domain.OrderReceived
-import jakarta.transaction.Transactional
-import org.springframework.context.ApplicationEventPublisher
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
-class OrderService(val orderRepository: OrderRepository,
-                   val publisher: ApplicationEventPublisher) {
+class OrderService(val orderRepository: OrderRepository) {
 
     @Transactional
     fun create(order: OrderEntity): OrderEntity {
         return orderRepository.save(order)
-                .also { publisher.publishEvent(OrderReceived(it)) }
     }
 
 }
